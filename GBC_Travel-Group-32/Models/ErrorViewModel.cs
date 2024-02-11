7cc40cccec35a0bc508401b44330c90065beb02c
WillingtonSt <145478368+WillingtonSt@users.noreply.github.com>
2024-02-11 12:34:46 -0500

    first commit

---
+++ GBC_Travel-Group-32/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace GBC_Travel_Group_32.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
